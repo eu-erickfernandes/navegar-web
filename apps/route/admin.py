c3948da656c012a93ff20881bcf48d6cf53af1c8
Érick Fernandes <erickfernandess2@gmail.com>
2024-07-13 17:16:16 -0500

    Modelagem de cidades e embarcações

--- apps/route/admin.py
+++ apps/route/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import City, Boat
+
 # Register your models here.
+class BoatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'supplier')
+    list_filter = ('name', 'supplier')
+    search_fields = ('name', 'supplier')
+
+admin.site.register(Boat, BoatAdmin)
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(City, CityAdmin)
--- apps/route/models.py
+++ apps/route/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+from apps.authentication.models import CustomUser
+
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+    
+
+class Boat(models.Model):
+    supplier = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return self.name
