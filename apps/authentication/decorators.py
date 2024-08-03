844b70488e7e28edbc80045715d8f38ef079458a
Érick Fernandes <erickfernandess2@gmail.com>
2024-08-03 17:54:52 -0500

    Dashboard Fixes #24

--- apps/authentication/decorators.py
+++ apps/authentication/decorators.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.http import Http404
 
 def redirect_authenticated_user(view):
     def wrapper(request, *args, **kwargs):
@@ -9,3 +10,16 @@ def redirect_authenticated_user(view):
         return view(request)
     
     return wrapper
+
+
+def required_user_roles(*roles):
+    def decorator(view):
+        def wrapper(request, *args, **kwargs):
+            if request.user.role in roles:
+                return view(request, *args, **kwargs)
+            
+            raise Http404
+            
+        return wrapper
+    
+    return decorator
